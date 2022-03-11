021c75ce883215e4e13c5e90b2dc14ef35caa604
Alexandre <zenika@MacBook-Pro-de-ZENIKA.local>
2022-03-11 15:32:40 +0100

    form done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,89 @@
-<script setup>
+<script lang="ts">
+import { ref, watch } from 'vue';
+import { Form, Field, useField, ErrorMessage} from 'vee-validate';
+import * as Yup from 'yup'
+
+export default {
+  //  setup() {
+  //     const usr= useField('username', Yup.string()
+  //             .required("Username is required")
+  //             .max(15, "Username is too long"));
+  //     const username = usr.value
+  //     const errorUsr = usr.errorMessage
+  //     const pwd= useField('password', Yup.string()
+  //             .required("Password is required")
+  //             .min(6, "Password must contain a minimum of 6 characters"));
+  //     const password = pwd.value
+  //     const errorPwd = pwd.errorMessage
+  //   return {
+  //     username,
+  //     errorUsr,
+  //     password,
+  //     errorPwd,
+  //   };
+  // },
+  components: {
+    Form,
+    Field,
+    ErrorMessage,
+  },
+  data(){
+    const validUsr = Yup.string()
+              .required("Username is required")
+              .max(15, "Username is too long")
+    const validPwd = Yup.string()
+              .required("Password is required")
+              .min(6, "Password must contain a minimum of 6 characters")
+    //const validUsr = this.usename;
+    console.log(validUsr)
+    // const validPwd = this.password;
+    return {
+      validUsr,
+      validPwd,
+    };
+  },
+
+  methods: {
+    onSubmit(values : []) {
+      //console.log(values)
+      this.$router.push(`/generator`);
+    },
+  },
+};
 </script>
 
 <template>
-  <main>
-    <div class="home-container">
+  <div class="home-container">
       <div class="login-container">
-          <form>
-            <div class="input-container">
-              <label>Username</label>
-              <input 
-                name="username"
-                type="text" 
-                placeholder="Enter your username"
-              />
-            </div>
-
-            <div class="input-container">
-              <label>Password</label>
-              <input 
-                name="password"
-                type="password" 
-                placeholder="Enter your password"
-              />
-            </div>
-
-            <div class="btn-container">
-              <button type="submit" class="btn-submit">Login</button>
-            </div>
-          </form>
-      </div>
+      <Form @submit="onSubmit" >
+          <div class="input-container">
+                  <label htmlFor="username" >Username</label>
+                  <Field 
+                      name="username"
+                      type="text" 
+                      placeholder="Enter your username"
+                      :rules = "validUsr"
+                  />
+                  
+                <p><ErrorMessage name="username" /></p>
+          </div>
+          <div class="input-container">
+                  <label htmlFor="password" >Password</label>
+                  <Field 
+                      name="password"
+                      type="password" 
+                      placeholder="Enter your password"
+                      :rules = "validPwd"
+                  />
+                  <p><ErrorMessage name="password" /></p>
+          </div>
+          <div class="btn-container">
+            <button class="btn-submit">Login</button>
+          </div>
+          
+      </Form>
     </div>
-  </main>
+  </div>
 </template>
 
 <style scoped>
